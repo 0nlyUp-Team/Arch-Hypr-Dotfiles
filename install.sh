69c120a0ac1d8b7870d2960bc5c27f2d3659cd2b
twozeroonesix <k4128029@gmail.com>
2025-06-19 10:33:45 +0500

    updated Install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-
+set -e
 PACMAN_PKGS=(ttf-nerd-fonts-symbols neovim brightnessctl noto-fonts-emoji python-requests less base-devel github-cli rsync ncdu qt5-wayland qt6-wayland qt5ct kvantum kvantum-qt5 nwg-look hyprland net-tools alacritty python3 python-pip btop fastfetch waybar rofi cliphist wl-clipboard networkmanager nm-connection-editor swaync swaybg swww hyprpaper hyprlock pavucontrol font-manager ttf-font-awesome ttf-nerd-fonts-symbols aria2 unrar file-roller thunar tumbler gvfs git mousepad)
 AUR_PKGS=(apple-fonts cava wlogout python-pywal16 librewolf-bin nwg-look mpvpaper ffmpegthumbnailer hyprshot qt6ct-kde)
 
 install_aur_helper() {
+    if [[ -x /sbin/yay || -x /usr/bin/yay ]]; then
+        return
+    fi
   sudo pacman -S --needed --noconfirm git base-devel
   git clone https://aur.archlinux.org/yay.git /tmp/yay
   (cd /tmp/yay && makepkg -si --noconfirm)
 }
 
 
-
-
 install_paru() {
+    if [[ -x /sbin/paru || -x /usr/bin/paru ]]; then
+        return
+    fi
     if ! sudo pacman -S --needed --noconfirm git base-devel; then
         echo "[!] Failed to install dependencies" >&2
         return 1
@@ -70,6 +74,28 @@ install_ohmybash() {
   fi
 }
 
+install_ohmyzsh() {
+    if ! command -v zsh &>/dev/null; then
+        echo "Installing zsh..."
+        sudo pacman -S --noconfirm zsh || {
+            echo "Failed to install zsh"
+            return 1
+        }
+    else
+        echo "Zsh Installed"
+    fi
+
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        RUNZSH=no KEEP_ZSHRC=yes CHSH=no bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || {
+            echo "Failed to Install Oh My Zsh"
+            return 1
+        }
+        echo "Oh My Zsh Installed"
+    fi
+
+    echo "Shell: $SHELL"
+}
+
 setup_language() {
     echo "→ Uncommenting ru_RU.UTF-8 in /etc/locale.gen"
     sudo sed -i 's/^#\?ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen || {
@@ -107,6 +133,14 @@ wait(){
 }
 
 main() {
+  install_ohmyzsh
+  wait
+  install_ohmybash
+  wait
+  setup_language
+  wait
+  copy_configs
+  wait
   install_aur_helper
   wait
   install_paru
@@ -114,12 +148,6 @@ main() {
   install_packages
   wait
   fix_xdg_portal_hypr
-  wait
-  install_ohmybash
-  wait
-  setup_language
-  wait
-  copy_configs
   wait
   hyprctl reload
   echo "Installed!"
